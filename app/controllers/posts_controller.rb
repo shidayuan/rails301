9b78d0db11217da8d3fedbe2943b455e6b32e747
shichengyuan <15036062755@163.com>
2018-05-18 16:09:13 +0800

    implement my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  @group = Group.find(params[:group_id])
+  @post = Post.find(params[:id])
+end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path flash[:notice] = "Update success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path flash[:warning] = "Delete success"
+  end
+  
+
+
   private
   def post_params
     params.require(:post).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,13 @@ Rails.application.routes.draw do
     member do
       post :join
       post :quit
-    end 
+    end
     resources :posts
+  end
+  namespace :account do
+    resources :groups
+    resources :posts 
+
   end
   root 'groups#index'
 end
